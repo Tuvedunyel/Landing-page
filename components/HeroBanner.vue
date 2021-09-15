7f287c62d291dd93c5dbae9eecc88806506ce42e
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-09-15 08:50:55 +0200

    first commit

---
+++ components/HeroBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <div class="container">
+      <img src="../static/img/logo.svg" alt="Btg communication" class="logo" />
+      <h1>Coucou</h1>
+      <p>test</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <HeroBanner />
+</template>
+
+<script>
+export default {}
+</script>
