583f79f2fe92b9821b343fb763a0808c238b032d
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-09-15 12:13:47 +0200

    struncture no mobile

--- components/Structure.vue
+++ components/Structure.vue
@@ -1,5 +1,69 @@
 <template>
-  <div class="container-narrow"></div>
+  <div class="no-start container-narrow">
+    <section class="hero-title">
+      <h2>
+        Quelque soit<br /><span class="bold">La taille de votre structure</span
+        ><br /><span class="yellow"
+          >vous méritez une communication à votre image</span
+        >
+      </h2>
+    </section>
+    <section class="offrons">
+      <img
+        src="../static/img/blackbullet-left.png"
+        alt="image-decorative"
+        class="left-image"
+      />
+      <div class="offre-container">
+        <h3 class="etiquette">Nous offrons</h3>
+        <ul class="offre">
+          <li><span class="white">1 audit</span> de votre communication</li>
+          <li><span class="white">1 Étude complète</span> par nos experts</li>
+          <li><span class="white">Des recommandations</span> sur mesure</li>
+          <li><span class="white">1 document</span> de restitution</li>
+          <li><span class="white">100%</span> offert</li>
+          <li><span class="white">Sans</span> engagement</li>
+        </ul>
+        <h3 class="etiquette">
+          Valeur: <span class="barre">800€</span> <span class="indice">ht</span>
+        </h3>
+      </div>
+      <img
+        src="../static/img/blackbullet-right.png"
+        alt="image-decorative"
+        class="right-image"
+      />
+    </section>
+    <section class="partenaires">
+      <h2>Ils nous font confiance</h2>
+      <ul class="partenaires-container">
+        <li>
+          <img src="../static/img/arianegroup.svg" alt="Logo Ariane group" />
+        </li>
+        <li>
+          <img src="../static/img/saint-gobain.svg" alt="Logo Saint Gobain" />
+        </li>
+        <li>
+          <img src="../static/img/st-michel.svg" alt="Logo Saint Michel" />
+        </li>
+        <li>
+          <img
+            src="../static/img/croix-rouge.svg"
+            alt="Logo Croix rouge française"
+          />
+        </li>
+        <li><img src="../static/img/audilab.svg" alt="Logo Audilab" /></li>
+        <li><img src="../static/img/mediprma.svg" alt="Logo médipréma" /></li>
+        <li>
+          <img
+            src="../static/img/credit_agricole.svg"
+            alt="Logo du Crédit Agricole"
+          />
+        </li>
+        <li><img src="../static/img/isover.svg" alt="Logo d'Isover" /></li>
+      </ul>
+    </section>
+  </div>
 </template>
 <script>
 export default {
